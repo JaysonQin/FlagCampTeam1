fe31b7568fa9316560c766138f67a504829c02ce
JaysonQin <qinzisen0123@gmail.com>
2025-05-11 17:16:31 -0400

    initial Spring Boot backend

---
+++ src/main/java/com.laioffer.flagcampbe/FlagcampbeApplication.java
@@ -0,0 +1,11 @@
+package com.laioffer.flagcampbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlagcampbeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlagcampbeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/laioffer/flagcampbe/FlagcampbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.flagcampbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlagcampbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
